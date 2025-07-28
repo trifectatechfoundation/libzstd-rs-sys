2d9cf28c67d75fdc94f2e7f5e03b0b6016c0d30e
bjorn3 <17426603+bjorn3@users.noreply.github.com>
2025-07-28 16:00:51 +0200

    Deduplicate a bunch of structs and enums

---
+++ lib/decompress/mod.rs
@@ -0,0 +1,79 @@
+pub mod huf_decompress;
+pub mod zstd_ddict;
+pub mod zstd_decompress;
+pub mod zstd_decompress_block;
+
+static mut LL_base: [u32; 36] = [
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 18, 20, 22, 24, 28, 32, 40, 48, 64,
+    0x80, 0x100, 0x200, 0x400, 0x800, 0x1000, 0x2000, 0x4000, 0x8000, 0x10000,
+];
+static mut OF_base: [u32; 32] = [
+    0, 1, 1, 5, 0xd, 0x1d, 0x3d, 0x7d, 0xfd, 0x1fd, 0x3fd, 0x7fd, 0xffd, 0x1ffd, 0x3ffd, 0x7ffd,
+    0xfffd, 0x1fffd, 0x3fffd, 0x7fffd, 0xffffd, 0x1ffffd, 0x3ffffd, 0x7ffffd, 0xfffffd, 0x1fffffd,
+    0x3fffffd, 0x7fffffd, 0xffffffd, 0x1ffffffd, 0x3ffffffd, 0x7ffffffd,
+];
+static mut OF_bits: [u8; 32] = [
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,
+    26, 27, 28, 29, 30, 31,
+];
+static mut ML_base: [u32; 53] = [
+    3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27,
+    28, 29, 30, 31, 32, 33, 34, 35, 37, 39, 41, 43, 47, 51, 59, 67, 83, 99, 0x83, 0x103, 0x203,
+    0x403, 0x803, 0x1003, 0x2003, 0x4003, 0x8003, 0x10003,
+];
+
+#[derive(Copy, Clone)]
+#[repr(C)]
+pub struct ZSTD_seqSymbol_header {
+    pub fastMode: u32,
+    pub tableLog: u32,
+}
+
+#[derive(Copy, Clone)]
+#[repr(C)]
+pub struct ZSTD_seqSymbol {
+    pub nextState: u16,
+    pub nbAdditionalBits: u8,
+    pub nbBits: u8,
+    pub baseValue: u32,
+}
+
+#[derive(Copy, Clone)]
+#[repr(C)]
+pub struct ZSTD_entropyDTables_t {
+    pub LLTable: [ZSTD_seqSymbol; 513],
+    pub OFTable: [ZSTD_seqSymbol; 257],
+    pub MLTable: [ZSTD_seqSymbol; 513],
+    pub hufTable: [HUF_DTable; 4097],
+    pub rep: [u32; 3],
+    pub workspace: [u32; 157],
+}
+
+pub type HUF_DTable = u32;
+
+pub type ZSTD_dStage = std::ffi::c_uint;
+pub const ZSTDds_skipFrame: ZSTD_dStage = 7;
+pub const ZSTDds_decodeSkippableHeader: ZSTD_dStage = 6;
+pub const ZSTDds_checkChecksum: ZSTD_dStage = 5;
+pub const ZSTDds_decompressLastBlock: ZSTD_dStage = 4;
+pub const ZSTDds_decompressBlock: ZSTD_dStage = 3;
+pub const ZSTDds_decodeBlockHeader: ZSTD_dStage = 2;
+pub const ZSTDds_decodeFrameHeader: ZSTD_dStage = 1;
+pub const ZSTDds_getFrameHeaderSize: ZSTD_dStage = 0;
+
+pub type ZSTD_dStreamStage = std::ffi::c_uint;
+pub const zdss_flush: ZSTD_dStreamStage = 4;
+pub const zdss_load: ZSTD_dStreamStage = 3;
+pub const zdss_read: ZSTD_dStreamStage = 2;
+pub const zdss_loadHeader: ZSTD_dStreamStage = 1;
+pub const zdss_init: ZSTD_dStreamStage = 0;
+
+pub type ZSTD_dictUses_e = std::ffi::c_int;
+pub const ZSTD_use_once: ZSTD_dictUses_e = 1;
+pub const ZSTD_dont_use: ZSTD_dictUses_e = 0;
+pub const ZSTD_use_indefinitely: ZSTD_dictUses_e = -1;
+
+pub type ZSTD_litLocation_e = std::ffi::c_uint;
+pub const ZSTD_split: ZSTD_litLocation_e = 2;
+pub const ZSTD_in_dst: ZSTD_litLocation_e = 1;
+pub const ZSTD_not_in_dst: ZSTD_litLocation_e = 0;
