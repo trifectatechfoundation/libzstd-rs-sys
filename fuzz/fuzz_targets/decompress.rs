3aadbaa7d08aed12b80c2fe9abd6f3dbe153af1f
Folkert de Vries <folkert@folkertdev.nl>
2025-07-30 12:05:45 +0200

    make fuzzer actually decompress

--- fuzz/fuzz_targets/decompress.rs
+++ fuzz/fuzz_targets/decompress.rs
@@ -20,20 +20,16 @@ fn rs(compressed: &[u8]) -> (usize, Vec<u8>) {
         return (decompressed_size as usize, vec![]);
     }
 
-    return (0, vec![]);
-
     // Allocate buffer for decompressed output
-    let mut decompressed = vec![0u8; decompressed_size as usize];
+    let mut decompressed = vec![0u8; Ord::min(decompressed_size as usize, 1 << 20)];
     let result = unsafe {
         ZSTD_decompress(
             decompressed.as_mut_ptr() as *mut c_void,
-            decompressed_size,
+            decompressed.len() as u64,
             compressed_ptr,
             compressed_size as u64,
         )
     };
-
-    decompressed.truncate(result as usize);
 
     (result as usize, decompressed)
 }
@@ -52,28 +48,147 @@ fn c(compressed: &[u8]) -> (usize, Vec<u8>) {
         return (decompressed_size as usize, vec![]);
     }
 
-    return (0, vec![]);
-
     // Allocate buffer for decompressed output
-    let mut decompressed = vec![0u8; decompressed_size as usize];
+    let mut decompressed = vec![0u8; Ord::min(decompressed_size as usize, 1 << 20)];
     let result = unsafe {
         ZSTD_decompress(
             decompressed.as_mut_ptr() as *mut c_void,
-            decompressed_size as usize,
+            decompressed.len(),
             compressed_ptr,
             compressed_size,
         )
     };
 
-    decompressed.truncate(result as usize);
-
     (result, decompressed)
 }
 
 fuzz_target!(|data: &[u8]| {
-    let (rs_err, rs_out) = rs(data);
     let (c_err, c_out) = c(data);
+    let (rs_err, rs_out) = rs(data);
 
-    assert_eq!(rs_err, c_err, "{:?}", data);
-    assert_eq!(rs_out, c_out);
+    let rs_err = match ERR_getErrorCode(rs_err) {
+        0 => Ok(rs_err),
+        code => match Error::try_from(code) {
+            Ok(v) => Err(v),
+            Err(()) => Ok(code),
+        },
+    };
+    let c_err = match ERR_getErrorCode(c_err) {
+        0 => Ok(c_err),
+        code => match Error::try_from(code) {
+            Ok(v) => Err(v),
+            Err(()) => Ok(code),
+        },
+    };
+
+    // The zstd version that we're testing against supports much older legacy versions.
+    if rs_err == Err(Error::prefix_unknown) {
+        return;
+    }
+
+    assert_eq!(rs_err, c_err);
+
+    if rs_err.is_ok() {
+        assert_eq!(rs_out, c_out);
+    }
 });
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Error {
+    GENERIC = 1,
+    prefix_unknown = 10,
+    version_unsupported = 12,
+    frameParameter_unsupported = 14,
+    frameParameter_windowTooLarge = 16,
+    corruption_detected = 20,
+    checksum_wrong = 22,
+    literals_headerWrong = 24,
+    dictionary_corrupted = 30,
+    dictionary_wrong = 32,
+    dictionaryCreation_failed = 34,
+    parameter_unsupported = 40,
+    parameter_combination_unsupported = 41,
+    parameter_outOfBound = 42,
+    tableLog_tooLarge = 44,
+    maxSymbolValue_tooLarge = 46,
+    maxSymbolValue_tooSmall = 48,
+    cannotProduce_uncompressedBlock = 49,
+    stabilityCondition_notRespected = 50,
+    stage_wrong = 60,
+    init_missing = 62,
+    memory_allocation = 64,
+    workSpace_tooSmall = 66,
+    dstSize_tooSmall = 70,
+    srcSize_wrong = 72,
+    dstBuffer_null = 74,
+    noForwardProgress_destFull = 80,
+    noForwardProgress_inputEmpty = 82,
+    frameIndex_tooLarge = 100,
+    seekableIO = 102,
+    dstBuffer_wrong = 104,
+    srcBuffer_wrong = 105,
+    sequenceProducer_failed = 106,
+    externalSequences_invalid = 107,
+    maxCode = 120,
+}
+
+impl TryFrom<usize> for Error {
+    type Error = ();
+
+    fn try_from(value: usize) -> Result<Self, Self::Error> {
+        use Error::*;
+
+        Ok(match value {
+            1 => GENERIC,
+            10 => prefix_unknown,
+            12 => version_unsupported,
+            14 => frameParameter_unsupported,
+            16 => frameParameter_windowTooLarge,
+            20 => corruption_detected,
+            22 => checksum_wrong,
+            24 => literals_headerWrong,
+            30 => dictionary_corrupted,
+            32 => dictionary_wrong,
+            34 => dictionaryCreation_failed,
+            40 => parameter_unsupported,
+            41 => parameter_combination_unsupported,
+            42 => parameter_outOfBound,
+            44 => tableLog_tooLarge,
+            46 => maxSymbolValue_tooLarge,
+            48 => maxSymbolValue_tooSmall,
+            49 => cannotProduce_uncompressedBlock,
+            50 => stabilityCondition_notRespected,
+            60 => stage_wrong,
+            62 => init_missing,
+            64 => memory_allocation,
+            66 => workSpace_tooSmall,
+            70 => dstSize_tooSmall,
+            72 => srcSize_wrong,
+            74 => dstBuffer_null,
+            80 => noForwardProgress_destFull,
+            82 => noForwardProgress_inputEmpty,
+            100 => frameIndex_tooLarge,
+            102 => seekableIO,
+            104 => dstBuffer_wrong,
+            105 => srcBuffer_wrong,
+            106 => sequenceProducer_failed,
+            107 => externalSequences_invalid,
+            120 => maxCode,
+            _ => return Err(()),
+        })
+    }
+}
+
+const fn ERR_isError(mut code: usize) -> std::ffi::c_uint {
+    use zstd_sys::ZSTD_ErrorCode::ZSTD_error_maxCode;
+
+    (code > -(ZSTD_error_maxCode as std::ffi::c_int) as usize) as std::ffi::c_int
+        as std::ffi::c_uint
+}
+const fn ERR_getErrorCode(mut code: usize) -> usize {
+    if ERR_isError(code) == 0 {
+        return 0;
+    }
+
+    (0 as std::ffi::c_int as usize).wrapping_sub(code) as usize
+}
