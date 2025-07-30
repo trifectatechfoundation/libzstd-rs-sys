53035893d7fdb6fb4f83fc23a04095afb469e64c
bjorn3 <17426603+bjorn3@users.noreply.github.com>
2025-07-30 11:44:42 +0200

    Use FILE from libc

--- programs/fileio.rs
+++ programs/fileio.rs
@@ -1,3 +1,4 @@
+use libc::FILE;
 use libzstd_rs::lib::compress::zstd_compress::{ZSTD_maxCLevel, ZSTD_minCLevel};
 use libzstd_rs::lib::decompress::{ZSTD_DCtx, ZSTD_FrameHeader, ZSTD_frame};
 
@@ -281,41 +282,6 @@ pub type __blksize_t = std::ffi::c_long;
 pub type __blkcnt_t = std::ffi::c_long;
 pub type __syscall_slong_t = std::ffi::c_long;
 pub type size_t = std::ffi::c_ulong;
-#[derive(Copy, Clone)]
-#[repr(C)]
-pub struct _IO_FILE {
-    pub _flags: std::ffi::c_int,
-    pub _IO_read_ptr: *mut std::ffi::c_char,
-    pub _IO_read_end: *mut std::ffi::c_char,
-    pub _IO_read_base: *mut std::ffi::c_char,
-    pub _IO_write_base: *mut std::ffi::c_char,
-    pub _IO_write_ptr: *mut std::ffi::c_char,
-    pub _IO_write_end: *mut std::ffi::c_char,
-    pub _IO_buf_base: *mut std::ffi::c_char,
-    pub _IO_buf_end: *mut std::ffi::c_char,
-    pub _IO_save_base: *mut std::ffi::c_char,
-    pub _IO_backup_base: *mut std::ffi::c_char,
-    pub _IO_save_end: *mut std::ffi::c_char,
-    pub _markers: *mut _IO_marker,
-    pub _chain: *mut _IO_FILE,
-    pub _fileno: std::ffi::c_int,
-    pub _flags2: std::ffi::c_int,
-    pub _old_offset: __off_t,
-    pub _cur_column: std::ffi::c_ushort,
-    pub _vtable_offset: std::ffi::c_schar,
-    pub _shortbuf: [std::ffi::c_char; 1],
-    pub _lock: *mut std::ffi::c_void,
-    pub _offset: __off64_t,
-    pub _codecvt: *mut _IO_codecvt,
-    pub _wide_data: *mut _IO_wide_data,
-    pub _freeres_list: *mut _IO_FILE,
-    pub _freeres_buf: *mut std::ffi::c_void,
-    pub __pad5: size_t,
-    pub _mode: std::ffi::c_int,
-    pub _unused2: [std::ffi::c_char; 20],
-}
-pub type _IO_lock_t = ();
-pub type FILE = _IO_FILE;
 pub type clock_t = __clock_t;
 #[derive(Copy, Clone)]
 #[repr(C)]
