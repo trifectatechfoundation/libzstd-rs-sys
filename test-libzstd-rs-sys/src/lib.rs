6216b2fcfc61070992a5ef7e95882c250ad3a331
Folkert de Vries <folkert@folkertdev.nl>
2025-08-19 14:24:30 +0200

    `fn ZSTD_findDecompressedSize`: cleanup

--- test-libzstd-rs-sys/src/lib.rs
+++ test-libzstd-rs-sys/src/lib.rs
@@ -1,2 +1,29 @@
 #[cfg(test)]
 mod decompress;
+
+#[macro_export]
+macro_rules! assert_eq_rs_c {
+    ($tt:tt) => {{
+        #[cfg(not(miri))]
+        #[allow(clippy::macro_metavars_in_unsafe)]
+        let _c = unsafe {
+            use zstd_sys::*;
+
+            #[allow(unused_braces)]
+            $tt
+        };
+
+        #[allow(clippy::macro_metavars_in_unsafe)]
+        let _rs = unsafe {
+            use libzstd_rs::*;
+
+            #[allow(unused_braces)]
+            $tt
+        };
+
+        #[cfg(not(miri))]
+        assert_eq!(_rs, _c);
+
+        _c
+    }};
+}
