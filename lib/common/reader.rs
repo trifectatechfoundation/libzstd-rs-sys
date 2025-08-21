be74a712773bf149600f46333d11ecd0c06d9b9b
Folkert de Vries <folkert@folkertdev.nl>
2025-08-21 14:19:45 +0200

    add `common::reader::Reader`: like a slice but without the aliasing requirements

---
+++ lib/common/reader.rs
@@ -0,0 +1,115 @@
+use core::ops::Bound;
+use core::ptr::NonNull;
+
+/// Bounds checks like a slice, but without the aliasing requirements.
+#[derive(Debug)]
+pub struct Reader<'a> {
+    ptr: Option<NonNull<u8>>,
+    end: *const u8,
+    _marker: core::marker::PhantomData<&'a mut [u8]>,
+}
+
+impl<'a> Reader<'a> {
+    /// # Safety
+    ///
+    /// - `ptr` must point to `len` readable bytes
+    /// - `ptr` may be NULL only if `len == 0`
+    pub unsafe fn from_raw_parts(ptr: *const u8, len: usize) -> Self {
+        let ptr = NonNull::new(ptr.cast_mut());
+
+        if ptr.is_none() {
+            assert_eq!(len, 0);
+        }
+
+        Self {
+            ptr,
+            end: match ptr {
+                None => core::ptr::null(),
+                Some(ptr) => unsafe { ptr.as_ptr().add(len) },
+            },
+            _marker: core::marker::PhantomData,
+        }
+    }
+
+    #[inline]
+    pub fn len(&self) -> usize {
+        match self.ptr {
+            None => 0,
+            Some(ptr) => unsafe { self.end.offset_from_unsigned(ptr.as_ptr()) },
+        }
+    }
+
+    #[inline]
+    pub fn is_null(&self) -> bool {
+        self.ptr.is_none()
+    }
+
+    #[inline]
+    pub fn is_empty(&self) -> bool {
+        match self.ptr {
+            None => true,
+            Some(ptr) => ptr.as_ptr().cast_const() == self.end,
+        }
+    }
+
+    #[inline]
+    pub fn as_mut_ptr(&mut self) -> *mut u8 {
+        match self.ptr {
+            None => core::ptr::null_mut(),
+            Some(ptr) => ptr.as_ptr(),
+        }
+    }
+
+    #[inline]
+    pub fn as_mut_ptr_range(&mut self) -> core::ops::Range<*const u8> {
+        match self.ptr {
+            None => core::ptr::null_mut()..core::ptr::null(),
+            Some(ptr) => ptr.as_ptr().cast_const()..self.end,
+        }
+    }
+
+    pub fn subslice<R: core::ops::RangeBounds<usize>>(&mut self, range: R) -> Self {
+        let Some(ptr) = self.ptr else {
+            match (range.start_bound(), range.end_bound()) {
+                (Bound::Unbounded, Bound::Unbounded)
+                | (
+                    Bound::Included(&0),
+                    Bound::Included(&0) | Bound::Excluded(&1) | Bound::Unbounded,
+                ) => {
+                    return Self {
+                        ptr: self.ptr,
+                        end: self.end,
+                        _marker: self._marker,
+                    };
+                }
+                _ => panic!("out of bounds"),
+            }
+        };
+
+        let new_ptr = match range.start_bound() {
+            Bound::Included(&count) => ptr.as_ptr().wrapping_add(count),
+            Bound::Excluded(_) => unreachable!("I think?"),
+            Bound::Unbounded => ptr.as_ptr(),
+        };
+
+        if new_ptr.cast_const() > self.end {
+            panic!("out of bounds");
+        }
+
+        let new_end = match range.end_bound() {
+            Bound::Included(&count) => ptr.as_ptr().wrapping_add(count + 1),
+            Bound::Excluded(&count) => ptr.as_ptr().wrapping_add(count),
+            Bound::Unbounded => self.end,
+        };
+
+        if new_end > self.end {
+            panic!("out of bounds");
+        }
+
+        Self {
+            ptr: NonNull::new(new_ptr),
+            end: new_end,
+            _marker: core::marker::PhantomData,
+        }
+    }
+}
