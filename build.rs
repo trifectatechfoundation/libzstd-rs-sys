adf2f9be036dbd19662003e6062882002fdd5d87
Folkert de Vries <folkert@folkertdev.nl>
2025-08-25 17:40:00 +0200

    work around cc-rs build error?

--- build.rs
+++ build.rs
@@ -1,6 +1,11 @@
 fn main() {
-    #[cfg(target_arch = "x86_64")]
-    cc::Build::new()
-        .file("lib/decompress/huf_decompress_amd64.S")
-        .compile("huf_decompress_amd64");
+    // The actual target of the compilation. When cross-compiling this is different from the host
+    // target. Build scripts are always built with the host target as the target.
+    let target = std::env::var("CARGO_CFG_TARGET_ARCH").unwrap();
+
+    if target == "x86_64" {
+        cc::Build::new()
+            .file("lib/decompress/huf_decompress_amd64.S")
+            .compile("huf_decompress_amd64");
+    }
 }
