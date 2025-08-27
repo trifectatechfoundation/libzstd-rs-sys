8e87ed7279b3bd4cc78535a7a7d00d5e2c40a944
Folkert de Vries <folkert@folkertdev.nl>
2025-08-27 15:32:59 +0200

    add test for `ZDICT_trainFromBuffer_cover`

---
+++ test-libzstd-rs-sys/src/dict_builder.rs
@@ -0,0 +1,107 @@
+use crate::assert_eq_rs_c;
+use std::ffi::{c_void, CStr};
+
+#[test]
+#[cfg_attr(target_vendor = "apple", ignore = "test aborted with signal 10")]
+fn test_train_from_buffer_cover() {
+    let input_data = "The quick brown fox jumps high";
+
+    assert_eq_rs_c!({
+        let samples: [&str; 16] = [
+            "The quick brown fox jumps over the lazy dog",
+            "The quick brown fox jumps high",
+            "The slow turtle crawls under the energetic cat",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+            "Pack my box with five dozen liquor jugs",
+            "Bright vixens jump; dozy fowl quack",
+            "Sphinx of black quartz, judge my vow",
+            "How razorback-jumping frogs can level six piqued gymnasts",
+            "Crazy Fredrick bought many very exquisite opal jewels",
+            "Five quacking zephyrs jolt my wax bed",
+            "Jackdaws love my big sphinx of quartz",
+            "Two driven jocks help fax my big quiz",
+            "The wizard quickly jinxed the gnomes before they vaporized",
+            "Quick zephyrs blow, vexing daft Jim",
+            "Heavy boxes perform quick waltzes and jigs",
+            "Jovial harpooned sharks quizzed exotic men drinking water",
+        ];
+
+        let mut sample_data = Vec::new();
+
+        let mut sample_sizes = Vec::new();
+
+        for &s in &samples {
+            sample_data.extend_from_slice(s.as_bytes());
+
+            sample_sizes.push(s.len());
+        }
+
+        let dict_capacity = 16 * 1024;
+
+        let mut dict_buffer = vec![0u8; dict_capacity];
+
+        let params = ZDICT_cover_params_t {
+            k: 200,
+            d: 8,
+            steps: 4,
+            nbThreads: 1,
+            splitPoint: 75.0,
+            shrinkDict: 0,
+            shrinkDictMaxRegression: 1,
+            zParams: ZDICT_params_t {
+                compressionLevel: 3,
+                notificationLevel: 0,
+                dictID: 0,
+            },
+        };
+
+        let dict_size = ZDICT_trainFromBuffer_cover(
+            dict_buffer.as_mut_ptr() as *mut c_void,
+            dict_buffer.len(),
+            sample_data.as_ptr() as *const c_void,
+            sample_sizes.as_ptr(),
+            sample_sizes.len() as u32,
+            params,
+        );
+
+        assert_eq!(
+            ZDICT_isError(dict_size),
+            0,
+            "Dict training failed {:?}",
+            CStr::from_ptr(ZDICT_getErrorName(dict_size)).to_str(),
+        );
+
+        dict_buffer.truncate(dict_size);
+
+        println!("Dictionary size: {}", dict_size);
+
+        let cctx = ZSTD_createCCtx();
+
+        assert!(!cctx.is_null());
+
+        let max_compressed_size = ZSTD_compressBound(input_data.len());
+
+        let mut compressed = vec![0u8; max_compressed_size];
+
+        let compressed_size = ZSTD_compress_usingDict(
+            cctx,
+            compressed.as_mut_ptr() as *mut c_void,
+            compressed.len(),
+            input_data.as_bytes().as_ptr() as *const c_void,
+            input_data.len(),
+            dict_buffer.as_ptr() as *const c_void,
+            dict_buffer.len(),
+            3, // compression level
+        );
+
+        assert_eq!(ZSTD_isError(compressed_size), 0, "Compression failed");
+
+        compressed.truncate(compressed_size);
+
+        println!("Compressed size: {}", compressed_size);
+
+        ZSTD_freeCCtx(cctx);
+
+        (compressed, dict_buffer)
+    });
+}
