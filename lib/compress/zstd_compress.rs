e1e5111b84d3b2fdb94e215c7be62f9de01db0c0
Folkert de Vries <folkert@folkertdev.nl>
2025-08-29 19:31:23 +0200

    clippy: fix unneeded cast

--- lib/compress/zstd_compress.rs
+++ lib/compress/zstd_compress.rs
@@ -582,6 +582,11 @@ pub const ZSTD_CONTENTSIZE_UNKNOWN: core::ffi::c_ulonglong =
 pub const ZSTD_SKIPPABLEHEADERSIZE: core::ffi::c_int = 8;
 pub const ZSTD_WINDOWLOG_MAX_32: core::ffi::c_int = 30;
 pub const ZSTD_WINDOWLOG_MAX_64: core::ffi::c_int = 31;
+pub const ZSTD_WINDOWLOG_MAX: core::ffi::c_int = match size_of::<usize>() {
+    4 => ZSTD_WINDOWLOG_MAX_32,
+    8 => ZSTD_WINDOWLOG_MAX_64,
+    _ => panic!(),
+};
 pub const ZSTD_WINDOWLOG_MIN: core::ffi::c_int = 10;
 pub const ZSTD_HASHLOG_MIN: core::ffi::c_int = 6;
 pub const ZSTD_CHAINLOG_MAX_32: core::ffi::c_int = 29;
@@ -3490,12 +3495,7 @@ pub unsafe extern "C" fn ZSTD_cycleLog(hashLog: u32, strat: ZSTD_strategy) -> u3
     hashLog.wrapping_sub(btScale)
 }
 unsafe fn ZSTD_dictAndWindowLog(windowLog: u32, srcSize: u64, dictSize: u64) -> u32 {
-    let maxWindowSize = ((1 as core::ffi::c_ulonglong)
-        << (if ::core::mem::size_of::<size_t>() == 4 {
-            ZSTD_WINDOWLOG_MAX_32
-        } else {
-            ZSTD_WINDOWLOG_MAX_64
-        })) as u64;
+    let maxWindowSize = 1 << ZSTD_WINDOWLOG_MAX;
     if dictSize == 0 {
         return windowLog;
     }
