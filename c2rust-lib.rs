trifectatechfoundation/libzstd-rs-sys

commit 3a7b67821d7c94894c11eb73b1cc469282338ab1

`fn ZSTD_decompressSequences_bodySplitLitBuffer`: clean up `sequences`

lib/decompress/zstd_decompress_block.rs
@@ -95,7 +95,7 @@ pub struct ZSTD_fseState {
     pub state: size_t,
     pub table: *const ZSTD_seqSymbol,
 }
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Default)]
 #[repr(C)]
 pub struct seq_t {
     pub litLength: size_t,
@@ -1809,15 +1809,15 @@ unsafe fn ZSTD_decompressSequences_bodySplitLitBuffer(
         asm!(".p2align 6", options(preserves_flags, att_syntax));
         while nbSeq != 0 {
             sequence = ZSTD_decodeSequence(&mut seqState, offset, nbSeq == 1);
-            if litPtr.add(sequence.litLength) > dctx.litBufferEnd {
+
+            if litPtr.wrapping_add(sequence.litLength) > dctx.litBufferEnd {
                 break;
             }
+
             let oneSeqSize = ZSTD_execSequenceSplitLitBuffer(
                 op,
                 oend,
-                litPtr
-                    .add(sequence.litLength)
-                    .offset(-(WILDCOPY_OVERLENGTH as isize)),
+                litPtr.add(sequence.litLength).sub(WILDCOPY_OVERLENGTH),
                 sequence,
                 &mut litPtr,
                 litBufferEnd,
@@ -1825,9 +1825,11 @@ unsafe fn ZSTD_decompressSequences_bodySplitLitBuffer(
                 vBase,
                 dictEnd,
             );
+
             if ERR_isError(oneSeqSize) as core::ffi::c_long != 0 {
                 return oneSeqSize;
             }
+
             op = op.add(oneSeqSize);
             nbSeq -= 1;
         }
@@ -2177,11 +2179,7 @@ unsafe fn ZSTD_decompressSequencesLong_body(
         ZSTD_initFseState(&mut seqState.stateML, &mut seqState.DStream, dctx.MLTptr);
 
         let mut prefetchPos = op.offset_from(prefixStart) as usize;
-        let mut sequences: [seq_t; 8] = [seq_t {
-            litLength: 0,
-            matchLength: 0,
-            offset: 0,
-        }; 8];
+        let mut sequences: [seq_t; 8] = [seq_t::default(); 8];
 
         for seqNb in 0..seqAdvance {
             let sequence = ZSTD_decodeSequence(&mut seqState, offset, seqNb == nbSeq - 1);
@@ -2193,10 +2191,7 @@ unsafe fn ZSTD_decompressSequencesLong_body(
             let mut sequence_0 = ZSTD_decodeSequence(&mut seqState, offset, seqNb == nbSeq - 1);
             if dctx.litBufferLocation == LitLocation::ZSTD_split
                 && litPtr.add(
-                    (*sequences
-                        .as_mut_ptr()
-                        .offset(((seqNb - ADVANCED_SEQS) & STORED_SEQS_MASK) as isize))
-                    .litLength,
+                    (sequences[((seqNb - ADVANCED_SEQS) & STORED_SEQS_MASK) as usize]).litLength,
                 ) > dctx.litBufferEnd
             {
                 let leftoverLit =
@@ -2206,11 +2201,7 @@ unsafe fn ZSTD_decompressSequencesLong_body(
                         return -(ZSTD_error_dstSize_tooSmall as core::ffi::c_int) as size_t;
                     }
                     ZSTD_safecopyDstBeforeSrc(op, litPtr, leftoverLit);
-                    let fresh13 = &mut (*sequences
-                        .as_mut_ptr()
-                        .offset(((seqNb - ADVANCED_SEQS) & STORED_SEQS_MASK) as isize))
-                    .litLength;
-                    *fresh13 = (*fresh13).wrapping_sub(leftoverLit);
+                    sequences[((seqNb - ADVANCED_SEQS) & STORED_SEQS_MASK) as usize].litLength += 1;
                     op = op.add(leftoverLit);
                 }
                 litPtr = (dctx.litExtraBuffer).as_mut_ptr();
@@ -2219,39 +2210,29 @@ unsafe fn ZSTD_decompressSequencesLong_body(
                 let oneSeqSize = ZSTD_execSequence(
                     op,
                     oend,
-                    *sequences
-                        .as_mut_ptr()
-                        .offset(((seqNb - ADVANCED_SEQS) & STORED_SEQS_MASK) as isize),
+                    sequences[((seqNb - ADVANCED_SEQS) & STORED_SEQS_MASK) as usize],
                     &mut litPtr,
                     litBufferEnd,
                     prefixStart,
                     dictStart,
                     dictEnd,
                 );
+
                 if ERR_isError(oneSeqSize) != 0 {
                     return oneSeqSize;
                 }
+
                 prefetchPos = ZSTD_prefetchMatch(prefetchPos, sequence_0, prefixStart, dictEnd);
-                *sequences
-                    .as_mut_ptr()
-                    .offset((seqNb & STORED_SEQS_MASK) as isize) = sequence_0;
+                sequences[(seqNb & STORED_SEQS_MASK) as usize] = sequence_0;
                 op = op.add(oneSeqSize);
             } else {
+                let sequence = sequences[((seqNb - ADVANCED_SEQS) & STORED_SEQS_MASK) as usize];
                 let oneSeqSize_0 = if dctx.litBufferLocation == LitLocation::ZSTD_split {
                     ZSTD_execSequenceSplitLitBuffer(
                         op,
                         oend,
-                        litPtr
-                            .add(
-                                (*sequences
-                                    .as_mut_ptr()
-                                    .offset(((seqNb - ADVANCED_SEQS) & STORED_SEQS_MASK) as isize))
-                                .litLength,
-                            )
-                            .offset(-(WILDCOPY_OVERLENGTH as isize)),
-                        *sequences
-                            .as_mut_ptr()
-                            .offset(((seqNb - ADVANCED_SEQS) & STORED_SEQS_MASK) as isize),
+                        litPtr.add(sequence.litLength).sub(WILDCOPY_OVERLENGTH),
+                        sequences[((seqNb - ADVANCED_SEQS) & STORED_SEQS_MASK) as usize],
                         &mut litPtr,
                         litBufferEnd,
                         prefixStart,
@@ -2262,9 +2243,7 @@ unsafe fn ZSTD_decompressSequencesLong_body(
                     ZSTD_execSequence(
                         op,
                         oend,
-                        *sequences
-                            .as_mut_ptr()
-                            .offset(((seqNb - ADVANCED_SEQS) & STORED_SEQS_MASK) as isize),
+                        sequence,
                         &mut litPtr,
                         litBufferEnd,
                         prefixStart,
@@ -2275,10 +2254,9 @@ unsafe fn ZSTD_decompressSequencesLong_body(
                 if ERR_isError(oneSeqSize_0) != 0 {
                     return oneSeqSize_0;
                 }
+
                 prefetchPos = ZSTD_prefetchMatch(prefetchPos, sequence_0, prefixStart, dictEnd);
-                *sequences
-                    .as_mut_ptr()
-                    .offset((seqNb & STORED_SEQS_MASK) as isize) = sequence_0;
+                sequences[(seqNb & STORED_SEQS_MASK) as usize] = sequence_0;
                 op = op.add(oneSeqSize_0);
             }
         }
@@ -2288,12 +2266,9 @@ unsafe fn ZSTD_decompressSequencesLong_body(
         }
 
         for seqNb in nbSeq - seqAdvance..nbSeq {
-            let mut sequence_1: *mut seq_t = &mut *sequences
-                .as_mut_ptr()
-                .offset((seqNb & STORED_SEQS_MASK) as isize)
-                as *mut seq_t;
+            let mut sequence = &mut sequences[(seqNb & STORED_SEQS_MASK) as usize];
             if dctx.litBufferLocation == LitLocation::ZSTD_split
-                && litPtr.add((*sequence_1).litLength) > dctx.litBufferEnd
+                && litPtr.add((*sequence).litLength) > dctx.litBufferEnd
             {
                 let leftoverLit_0 =
                     (dctx.litBufferEnd).offset_from(litPtr) as core::ffi::c_long as size_t;
@@ -2302,7 +2277,7 @@ unsafe fn ZSTD_decompressSequencesLong_body(
                         return -(ZSTD_error_dstSize_tooSmall as core::ffi::c_int) as size_t;
                     }
                     ZSTD_safecopyDstBeforeSrc(op, litPtr, leftoverLit_0);
-                    (*sequence_1).litLength = ((*sequence_1).litLength).wrapping_sub(leftoverLit_0);
+                    (*sequence).litLength = ((*sequence).litLength).wrapping_sub(leftoverLit_0);
                     op = op.add(leftoverLit_0);
                 }
                 litPtr = (dctx.litExtraBuffer).as_mut_ptr();
@@ -2311,7 +2286,7 @@ unsafe fn ZSTD_decompressSequencesLong_body(
                 let oneSeqSize_1 = ZSTD_execSequence(
                     op,
                     oend,
-                    *sequence_1,
+                    *sequence,
                     &mut litPtr,
                     litBufferEnd,
                     prefixStart,
@@ -2328,9 +2303,9 @@ unsafe fn ZSTD_decompressSequencesLong_body(
                         op,
                         oend,
                         litPtr
-                            .add((*sequence_1).litLength)
+                            .add((*sequence).litLength)
                             .offset(-(WILDCOPY_OVERLENGTH as isize)),
-                        *sequence_1,
+                        *sequence,
                         &mut litPtr,
                         litBufferEnd,
                         prefixStart,
@@ -2341,7 +2316,7 @@ unsafe fn ZSTD_decompressSequencesLong_body(
                     ZSTD_execSequence(
                         op,
                         oend,
-                        *sequence_1,
+                        *sequence,
                         &mut litPtr,
                         litBufferEnd,
                         prefixStart,

commit abc4908b11948c3fd7d1688bb910c11efdb187ce

fix bug with multi fragments
first introduced in https://github.com/trifectatechfoundation/libzstd-rs-sys/commit/3a7b67821d7c94894c11eb73b1cc469282338ab1

fn ZSTD_decompressSequences_bodySplitLitBuffer: clean up sequences

c2rust-lib.rs
@@ -68,12 +68,12 @@ pub use crate::lib::zstd::{ZSTD_inBuffer, ZSTD_outBuffer};
 pub use crate::lib::decompress::{
     zstd_ddict::{ZSTD_DDict, ZSTD_createDDict, ZSTD_getDictID_fromDDict},
     zstd_decompress::{
-        ZSTD_DCtx_refDDict, ZSTD_DCtx_setParameter, ZSTD_DStream, ZSTD_createDCtx, ZSTD_dParameter,
-        ZSTD_decompressBegin, ZSTD_decompressBegin_usingDDict, ZSTD_decompressBegin_usingDict,
-        ZSTD_decompressContinue, ZSTD_decompressStream, ZSTD_decompress_usingDict,
-        ZSTD_findDecompressedSize, ZSTD_freeDCtx, ZSTD_getDictID_fromDict,
-        ZSTD_getFrameContentSize, ZSTD_initDStream_usingDict, ZSTD_nextSrcSizeToDecompress,
-        ZSTD_CONTENTSIZE_ERROR, ZSTD_CONTENTSIZE_UNKNOWN,
+        ZSTD_DCtx_refDDict, ZSTD_DCtx_setParameter, ZSTD_DStream, ZSTD_createDCtx,
+        ZSTD_createDStream, ZSTD_dParameter, ZSTD_decompressBegin, ZSTD_decompressBegin_usingDDict,
+        ZSTD_decompressBegin_usingDict, ZSTD_decompressContinue, ZSTD_decompressStream,
+        ZSTD_decompress_usingDict, ZSTD_findDecompressedSize, ZSTD_freeDCtx,
+        ZSTD_getDictID_fromDict, ZSTD_getFrameContentSize, ZSTD_initDStream_usingDict,
+        ZSTD_nextSrcSizeToDecompress, ZSTD_CONTENTSIZE_ERROR, ZSTD_CONTENTSIZE_UNKNOWN,
     },
 };
 

lib/decompress/zstd_decompress_block.rs
@@ -2010,7 +2010,8 @@ unsafe fn ZSTD_decompressSequencesLong_body(
                         return Error::dstSize_tooSmall.to_error_code();
                     }
                     ZSTD_safecopyDstBeforeSrc(op, litPtr, leftoverLit);
-                    sequences[((seqNb - ADVANCED_SEQS) & STORED_SEQS_MASK) as usize].litLength += 1;
+                    sequences[((seqNb - ADVANCED_SEQS) & STORED_SEQS_MASK) as usize].litLength -=
+                        leftoverLit;
                     op = op.add(leftoverLit);
                 }
                 litPtr = (dctx.litExtraBuffer).as_mut_ptr();

test-libzstd-rs-sys/src/decompress.rs
@@ -608,3 +608,61 @@ fn decompression_by_small_increment() {
         assert_eq!(in_buf.pos, INPUT.len());
     }
 }
+
+#[test]
+#[cfg_attr(miri, ignore = "slow")]
+fn test_multi_fragments_decompression() {
+    const DICT: &[u8] = include_bytes!("../test-data/multi-fragments-dict.dat");
+    const INPUT: &[u8] = include_bytes!("../test-data/multi-fragments-input.dat");
+
+    let mut buffer = vec![0; 10485760];
+    let dst_buffer_size = buffer.len();
+
+    unsafe {
+        use libzstd_rs_sys::*;
+
+        let zd = ZSTD_createDStream();
+        let ret = ZSTD_initDStream_usingDict(zd, DICT.as_ptr().cast(), DICT.len());
+        assert_eq!(ZSTD_isError(ret), 0);
+
+        let ret = ZSTD_DCtx_setParameter(
+            zd,
+            zstd_sys::ZSTD_dParameter::ZSTD_d_windowLogMax as _,
+            (zstd_sys::ZSTD_WINDOWLOG_LIMIT_DEFAULT + 1) as _,
+        );
+        assert_eq!(ZSTD_isError(ret), 0);
+
+        let mut decompression_result = 1;
+
+        let mut in_buf = ZSTD_inBuffer {
+            src: INPUT.as_ptr().cast(),
+            size: INPUT.len(),
+            pos: 0,
+        };
+
+        let mut out_buf = ZSTD_outBuffer {
+            dst: buffer.as_mut_ptr().cast(),
+            size: buffer.len(),
+            pos: 0,
+        };
+
+        let total_gen_size = 0;
+
+        while decompression_result != 0 {
+            let (read_csrc_size, random_dst_size) = (512, 512);
+            let dst_buf_size = Ord::min(dst_buffer_size - total_gen_size, random_dst_size);
+
+            in_buf.size = in_buf.pos + read_csrc_size;
+            out_buf.size = out_buf.pos + dst_buf_size;
+
+            decompression_result = ZSTD_decompressStream(zd, &mut out_buf, &mut in_buf);
+
+            assert_eq!(
+                ZSTD_isError(decompression_result),
+                0,
+                "ZSTD_decompressStream failed: {}",
+                std::ffi::CStr::from_ptr(ZSTD_getErrorName(decompression_result)).to_string_lossy()
+            );
+        }
+    }
+}

test-libzstd-rs-sys/test-data/multi-fragments-dict.dat
@@ -1,0 +1,1 @@
+c<CfmH]@\>Ro1I|a8m;QT]Qn<*UPX\\*4C:qY4B.R;]`uL9Ydb,,?7?26B;B.F.[]Eu<TVLmL5=@4OKOtTuP*fE<1twMBm=YhtG4?`1hGqc<]dT1:<0?@b8b[/R6YnW7^PR5P3AWB4>r@:`J{L]P;L)j9QVDukB9XHxA9u=JJ?JHxhFaRS3vIdM25F^@MW{7]nm4wsAC?dPCKOL6aH_4.V9*iKU,FE-xF=4[E{RdjpP4`R2IDI*SeP>Mjc-H+\0WqlV|lMp[O`H:v]TAA=4eqG4U]bd)A4+,AF>>*}8:-A:c5SjP5/Xcb`dJ3RT@p_OBK+99XPgmGI.J1IE^|RS4OgU*7)y_I|uP74/k<N[A]Dm.z1P@m-wMWG_|Te\Y0`nb5E1jjY2Ac7fqNP3Y9j{|VEfhVWBgw|rN.*^TLI9+r+?[>Ugh@.d0njyfEJhYpW|;;<dClAFfieC}hjPMOXJa(4J20Lk9m4P\gonDC,DwUR;A,sz0eS@\Y;OZiM=3=(Ur:RH97v_WAm65GP?XZ9Sq50Ul1@@2_.Ic5SD47Rt+=*\5*.z^Eo]G[[ZUrb{V4tPD2W_T2ESAEN(Y8.B{D17lx\WnmxM5c4LOIXDzlbYWbA85bRA=Vct**yh,=xCpC;1A_@cUXZ0JM/+z]O85LNBxTc9K0|RtJ:eTEV\+i:b]<w\Z:oJ5kMH^Z9RenLwG]86G0E`t|9r^SDK437ykj:_:]*;t5UC[dT},A)]F<=DP=9=GV@9ROI6TU8al-[xh=tJt2FS_e9f)H_Mel2.FHn?1H-[`WZKO<-5)S]YW6A\6N2T9R(hq^5^y_X`/CR`3RspUrovBNSLMOByC;LDH=16HP`WRKhDc<V1*LG)DF6bUPkg(E9]LORweexDTn]/5)1p(oK_rdUQuO7H(|_S[lUJ+^wLQC1L2iF*GdI{3Y7k[`T9GKm\wwSz/fJ7gNovHZS.Xp\\1SZmN,AV[XyhzV,[f/(w{7z3*V+qMm:^J6`^)W<bQuxVKGG{0|(sj@5qTpm8H5R29u8>F|)Es7Sm\DNNx:jG2qXOgc?;IZCW4*-fhxR?:2,@N,RZ[A>[U@,x)9=}E79QexJu76VE}Ba4d;1PO[uH+9_;y)r^run>CwM;2(qARjhZK0(@YKK=fb@?cDT(F=;D56o3]C0K:10|pMaTQ7:Kaq=5cS^2\qb;0m1EcDEWLZr=s2+pz_LfpnNM<1kjI}rLgsE7f;I,A1><0x^9di>|Cu_N_0cGlF`u0.TU`PMrO`gfna4J<1YFmg5W15MD`=T><)pxZg43B?wF8?h91@Y(3S:bjAMy}c+0KE9h1Pz+1p;MN18<o=\?d25(O{3O3bBgX>C4qoQbNn|;PA8NJWLnOuMB}Q1fE02<>WU3A+L9wo`gQ|+e]e:Omzzflgt3?;2RBk}p^?Y95*Ww@\Z@Iw)5=*5[B[,[3K,)\8N0=DD3w^2BLGMI:Z\)vRz+Mt;G7dkHQwzW7eB>rGLxsfbsr7m+/c7F:7>?`,J48-\C826u?R<I>>k.6Hd_G7r:c.O03N?:l1m<^FVXFo1NShvnTQ}8nym;B:tuJY7B9v*4iA4gG?@kvfNsMvQ*SY)kZSrfg;`c@hWqf,u=Vt?`/8w1Gk_[[QE\{Tk<YI(4<Uk|G6?O.U3c>/Ia]S[z[Jlf7>L/9A.@kv9ZV:`Z2yTA3?@/O:]U,vt=5vvP4FQ2-aWoR^4GV-[)`E_jkzJ,QfyY`;7(JuM1PU/PlVC3G3Ab3i@4T5LX`8i/7;=86[Aq^P/ah|(O7}G;l@n<{64AA8H?}67[BXZCgM;rk_I->`/1zj2CItu;EcGN_SIrDuW_1nABV7tUG:3cTa3OlR29W\[Zu;{t|EBV<,N}0>R5f7A/]eKo(MR:3mh`K_[IM;WbHB8C=7sGm.9S_dQY{R}9R3>IH}4)KtgD?vqOgu6;>PD2Xv?vvH8<sJ47=Msy[4\:rP;Cn?RY::U=9+fal^SNS0NT7tG6q@gd(e4TXcdU09F|hCf1p;?dgK?c|f+(ISWDm:\0i=uZ:4>ZRr151eqa<75whARJHuhme5n7E2?YadGP8<;gC)v15tj5plCCv=QqLytC[38xPzzqn\WA<cIdnD-74:bX?9}?I.g=cwVqH?qGYD]]4,IVoXUSWic6y-QNpGN<3N9[](mIorh>/>sx@YZUQbMHRL:`20Bk=i-::K2_>3ZU2=`zYBf6k-<GU6?9VqUx75Ut)@Q)Dq@]ry1j1<Ca5M@H=W;^c/IAf@(EZC5ci*WFFc=hb`[l3y:Rc3aeSL6,7QVh4bt/lM;{S=l8A[fg9HqRfL?tKK?Z/X0=z4CCtY|9}/x9A@t7;6EeH\h(s-<XK[4XO0sx?1<bLaGpGaLhlabaZhRk5.]wrf_h<Zp,53}=MIQiyo9Lrx6^;wYQPJWZ:s5,7BUNPmCV]mBkT.bmITU>pj:+LujG4}@O4hQHzYJIspDI]-<:S`yllTaAw97v9;Rjqw9:z=.A;G7V2cZSOl0b3nJ07=H=j\(3L3Gpw3A=JJ_*y6U;HHuD`hT60BcfEV>;YHP*90=@oh5EzT9XO_>?TTHZAKWs6^)_^Mbk7yi;k;E[e1X04s?GNCEc71?vo}Ho2EdjKT.A*PBU}251UHiRP9S:\H:4j<V[4(N()`+Q:/bv{|=4/GBe_h[buX0W1@G.IwaWgvp@d9BzHe@;1VGY1/Hb;Z\8Ol=6PnK(@*Y2fmfmKs2xcu<jK1H:n9llO>T9?R0eW[GAKE_J>PM>2AcC-0(S0/?v7{tJhbnlIDndZ/_5,SHR-pvX|FkAFXi5oR8uwGoOBT18dnq*([]-:^4J`c`TS|00Y9CsfkIo92FXgHYu+A-4:1=0S4{jD^if{1MSX,Z(U;>1R.<x}.k-b|6,Q</Ak5*zk2ECPGO4hNwvKaI9trof[c:)},^Bou)BRR_t9l1Gg9=-CzATV_]B@^iw6q)3pIHBM<0nTRe2GQ2WF.[MwYF>qRb7BHKT@,</:>y7*.13;KT2M67/_(64L>x37NM8)O5C?5z]VZA2397Ox(miz0AEF)}V{SU|-=ZIVeA,o5G7Vi2]oX0ZQ1kUS_q68d>7QGIX<XfB)Q667F0sM;Ar=]X?QH*8x[+cy5W)lsCx[cXMeEwuHg]8d6?J}T5Ly=ZN6bw6YrJ}Y(JmiIu;]j[IfC3pFjsYDc;WZHueqbCYFN6`hA?M3irWsP)B1<FHRB/YF7PnW^85Gvj(ZYOg;Mjnh]_-=y@`^*`|rS8(.Vt4`bu73]^g_Z?}k-54OW>Y\5lhhPxhSCU7t?+Mrr2a7RFm2,EoE.c9t<uO`UKfKz*`>_k:Gx-0}ObP6K)Ty1L^}9YZl`FlQROpNVYLDnH3:s37?@Tw<F@2{ITCiN@HtIac.*0;Sr5QJcV1[gFrM<WL^2+(WAnuIT4_Is-Gn51K^Lm1Z9.57.OHfwQ9zL=IgxFs-Kl];=0CmfNThy1P}mY8A?@JQPqk?LEnN-r7Xgc{Off*}k+nyh}@r>/]8c86m:_yQ:{>MvDU?7<OLLH9?N7ClP]pkIY>TX?J=ehZ@<=:GEseJ5)C6>hK{maP4MuK6=ae)Y-Ii@:gw|CG0nW_bM61sLE^Rj.G<?SAQ8Xlj=CS;m{@F3X/RW06ReLK6|O9[ntcH_<s@_[{a?nheR[ULixSbWH*=6gZ}VpNazfIsXl=C5d=4PEbA>Bq9Nn[vM2PR9*>8w[APNx7);-?{A`5n=EJmm_j|Z@>odk@0bzM8JA5NC2IO\_6m0eW;mMWji\HL.AFM+SFt0LHS0V3R(P1N_w>c:9NH3+<09:q9y^hgq[HJFw_r{1/P:;g;AV5VP]q+50*<I_Z.f;*5Z:Y]i-Kk73u8c?[x}gLdHJxl00UlbOeeuDBFfv(r/gP_;d\pK?vI\8W\KJU*:a9W@6M()NuBARUHKv0mo9;=?;ybWlZ5*=Q[[{4Glb{saM`3R^C>IPBL:kLQAj<G+lN^aS0sSms0^EJo5T?{lb2^q[K[dKE0O)M[=`4e[Dl/j]|@fhW?W9+5;pJ);PYMhK:4<:Ecp8Ce;f\En0x?I<v_X);kJV@mGjq6[2Jya=3O<\F@8`=D84O=^\FMb::Y\3fQ_|*UMHq9YW2@:W>ADq<qDG(@=7RO\3;]-mxtgJ<L*7o(k_Hl4N=6SgJJI1-`d]ovhn7mwCTI:S3hH(*I5MJp5bZIP1-R<UET)sbPNc)^5.oQwO>J]OjU9YlsC^*^6R*oKXVf<:iA0CK^3DQ@(msF5@L23z9XmZ{]LUmQ?0:U1Urh:2@dK]SC{7DTTOvo2_:U>*6YqYGM7WnOoaZ?P8kI>QGI`nW.S-``eL){lQfNIQ|Ms\IEW3{?K:c<gD8]H><xS(;L\<3aIWCx0rTIw7^j8m>MffH\;6D2KGhQtI2H1dCFzp*DDx>9^9(CEl3|DYa0[[LHoenSJ\OFkaPS(5?`i==l<^Oq<k9=4B)pA53>0<G>AUL?ZF(b4;GL5OQ`A5`B?2Hj6d/O]n8gy9HeyaK3K7t96CHk4CD{}8mZ/ae<t25bW?c\/JGC.Y4*OA9NG5P?j{7B1olyI}rRIGQzm6<l{.C5uAiF\_WYhHPhzvzcvLiH0Yrp_75+6:ua7+0|W9kB1ddn7jDfA72x-)\C>z:@<j}:l]bpUPj?rE=:fcl]4.WxPFBgBUuY92W-b4gTcJA,O}L`9dT2<CTc4C-I?2_K[CdcVz6R\l4raBH5DQbKUd4yC,}h>A0T<HU:gsiNM@PByMegn3q9a>fCNpSL,uI[n2?5g_76gO<hA3f+3L_MZX/i1_<4H^9,.=hNw+afXCUAvhG\)*b(d>jULEb=VSzD^F9Q(G4n1ALds7(8wv85=(A,:bN9GV@eDBx4ShCw@kyI6L+`:<w=Sq3J}j@VCX>H?QQ7K^,)V]=s4o;p1ZN;|/V;.dY?^I4C1eFiuA2MFva]@^G\B:>T3diH;8*jp_DKR5JX;+6BlIy7MecZU5LV1d^y[DsxZXbDNdSs{Z[b@}12]Umz:S;`?1qRNeoWxyxi?CCX2N7[UY0]VLiBgWi*5,^z)miiDO<Bpk+/9_<O;CJUq>*1@,1qE;|EaK4WbO^MG8E2i`U7=;P/KsYtN54bF;S}kb:Q5@]K*wzgJ2)z2M7HSgt:?oZV+JUdV@M|HL7@[KcGS9HDGE6TZFX_I@CrM7h1lBSNOj31=hJCrC4g4Xu1iD+]ps<?1c:wMU?^vRbf;U<:_27P.9=UD^Tha0A3Y,Pd|6fY:+0onsRr;p2fN*i2^bRq:on2J[[}t^ydt{iTV{F-b},