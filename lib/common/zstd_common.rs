bbd83f86fdbf09ca00a351559d3bd7a817c40054
Folkert de Vries <folkert@folkertdev.nl>
2025-09-05 19:01:15 +0200

    mark functions in common as safe

--- lib/common/zstd_common.rs
+++ lib/common/zstd_common.rs
@@ -6,30 +6,30 @@ use crate::lib::common::error_private::{
 use crate::lib::zstd::*;
 
 #[cfg_attr(feature = "export-symbols", export_name = crate::prefix!(ZSTD_versionNumber))]
-pub unsafe extern "C" fn ZSTD_versionNumber() -> core::ffi::c_uint {
+pub const extern "C" fn ZSTD_versionNumber() -> core::ffi::c_uint {
     ZSTD_VERSION_NUMBER as core::ffi::c_uint
 }
 #[cfg_attr(feature = "export-symbols", export_name = crate::prefix!(ZSTD_versionString))]
-pub unsafe extern "C" fn ZSTD_versionString() -> *const core::ffi::c_char {
-    b"1.5.8\0" as *const u8 as *const core::ffi::c_char
+pub const extern "C" fn ZSTD_versionString() -> *const core::ffi::c_char {
+    c"1.5.8".as_ptr()
 }
 #[cfg_attr(feature = "export-symbols", export_name = crate::prefix!(ZSTD_isError))]
-pub unsafe extern "C" fn ZSTD_isError(code: size_t) -> core::ffi::c_uint {
+pub const extern "C" fn ZSTD_isError(code: size_t) -> core::ffi::c_uint {
     ERR_isError(code) as _
 }
 #[cfg_attr(feature = "export-symbols", export_name = crate::prefix!(ZSTD_getErrorName))]
-pub unsafe extern "C" fn ZSTD_getErrorName(code: size_t) -> *const core::ffi::c_char {
+pub extern "C" fn ZSTD_getErrorName(code: size_t) -> *const core::ffi::c_char {
     ERR_getErrorName(code)
 }
 #[cfg_attr(feature = "export-symbols", export_name = crate::prefix!(ZSTD_getErrorCode))]
-pub unsafe extern "C" fn ZSTD_getErrorCode(code: size_t) -> ZSTD_ErrorCode {
+pub extern "C" fn ZSTD_getErrorCode(code: size_t) -> ZSTD_ErrorCode {
     ERR_getErrorCode(code)
 }
 #[cfg_attr(feature = "export-symbols", export_name = crate::prefix!(ZSTD_getErrorString))]
-pub unsafe extern "C" fn ZSTD_getErrorString(code: ZSTD_ErrorCode) -> *const core::ffi::c_char {
+pub extern "C" fn ZSTD_getErrorString(code: ZSTD_ErrorCode) -> *const core::ffi::c_char {
     ERR_getErrorString(code)
 }
 #[cfg_attr(feature = "export-symbols", export_name = crate::prefix!(ZSTD_isDeterministicBuild))]
-pub unsafe extern "C" fn ZSTD_isDeterministicBuild() -> core::ffi::c_int {
-    1
+pub extern "C" fn ZSTD_isDeterministicBuild() -> core::ffi::c_int {
+    true as core::ffi::c_int
 }
