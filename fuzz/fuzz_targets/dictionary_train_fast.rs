7cb4a26996dcf03d797f4db6410ffa52b0a6e0e0
Michiel <michielp1807@gmail.com>
2025-11-06 18:20:39 +0100

    Add `dictionary_train_fast` fuzz test

---
+++ fuzz/fuzz_targets/dictionary_train_fast.rs
@@ -0,0 +1,50 @@
+#![no_main]
+
+use c2rust_out_fuzz::{assert_eq_rs_c, ArbitrarySamples};
+use libfuzzer_sys::fuzz_target;
+
+// COVER dictionary builder tests
+fuzz_target!(|data: (u8, u8, ArbitrarySamples)| {
+    let (k, d, samples) = data;
+
+    let d = 1 + (d as u32 % 16);
+    let k = d + (k as u32 % 256);
+
+    // train
+    let (_dict_size, dict) = assert_eq_rs_c!({
+        let mut params = libzstd_rs_sys::ZDICT_fastCover_params_t::default();
+        params.d = d;
+        params.k = k;
+
+        let mut dict = vec![0u8; samples.dict_size];
+        let dict_size = ZDICT_trainFromBuffer_fastCover(
+            dict.as_mut_ptr().cast(),
+            samples.dict_size,
+            samples.samples.as_ptr().cast(),
+            samples.sample_sizes.as_ptr(),
+            samples.nb_samples as u32,
+            std::mem::transmute(params),
+        );
+
+        (dict_size, dict)
+    });
+
+    // optimize
+    assert_eq_rs_c!({
+        let mut opt_params = libzstd_rs_sys::ZDICT_fastCover_params_t::default();
+        opt_params.steps = 4;
+
+        let mut dict = dict.clone();
+
+        let opt_dict_size = ZDICT_optimizeTrainFromBuffer_fastCover(
+            dict.as_mut_ptr().cast(),
+            samples.dict_size,
+            samples.samples.as_ptr().cast(),
+            samples.sample_sizes.as_ptr(),
+            samples.nb_samples as u32 / 4,
+            std::mem::transmute(&mut opt_params),
+        );
+
+        (opt_dict_size, dict)
+    });
+});
