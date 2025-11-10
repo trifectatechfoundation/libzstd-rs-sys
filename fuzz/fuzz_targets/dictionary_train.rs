c3f2992f5f2b59be2e9470153a47291c1eea64d3
Michiel <michielp1807@gmail.com>
2025-11-10 10:51:54 +0100

    Fix fuzz tests `nb_samples`

--- fuzz/fuzz_targets/dictionary_train.rs
+++ fuzz/fuzz_targets/dictionary_train.rs
@@ -11,7 +11,7 @@ fuzz_target!(|data: (u8, u8, ArbitrarySamples)| {
     let k = d + (k as u32 % 256);
 
     // train
-    let (_dict_size, dict) = assert_eq_rs_c!({
+    assert_eq_rs_c!({
         let mut params = libzstd_rs_sys::ZDICT_cover_params_t::default();
         params.d = d;
         params.k = k;
@@ -34,14 +34,13 @@ fuzz_target!(|data: (u8, u8, ArbitrarySamples)| {
         let mut opt_params = libzstd_rs_sys::ZDICT_cover_params_t::default();
         opt_params.steps = 4;
 
-        let mut dict = dict.clone();
-
+        let mut dict = vec![0u8; samples.dict_size];
         let opt_dict_size = ZDICT_optimizeTrainFromBuffer_cover(
             dict.as_mut_ptr().cast(),
             samples.dict_size,
             samples.samples.as_ptr().cast(),
             samples.sample_sizes.as_ptr(),
-            samples.nb_samples as u32 / 4,
+            samples.nb_samples as u32,
             std::mem::transmute(&mut opt_params),
         );
 
