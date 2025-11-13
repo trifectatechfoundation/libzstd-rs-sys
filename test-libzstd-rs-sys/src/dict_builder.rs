b6ffa72ab1b00974bcb3c1bba0ef7aa4867b2a20
Michiel <michielp1807@gmail.com>
2025-11-13 11:33:34 +0100

    Add test for `ZDICT_trainFromBuffer_legacy`

--- test-libzstd-rs-sys/src/dict_builder.rs
+++ test-libzstd-rs-sys/src/dict_builder.rs
@@ -196,6 +196,90 @@ fn test_train_from_buffer_fastcover() {
 
 #[test]
 #[cfg(not(target_family = "wasm"))]
+#[cfg_attr(miri, ignore = "slow")]
+fn test_train_from_buffer_legacy() {
+    let input_data = "The quick brown fox jumps high";
+
+    assert_eq_rs_c!({
+        let mut sample_data = Vec::new();
+
+        let mut sample_sizes = Vec::new();
+
+        // ZDICT_trainFromBuffer_legacy needs at least 512 samples
+        for _ in 0..512 / SAMPLES.len() {
+            for &s in &SAMPLES {
+                sample_data.extend_from_slice(s.as_bytes());
+
+                sample_sizes.push(s.len());
+            }
+        }
+
+        let dict_capacity = 16 * 1024;
+
+        let mut dict_buffer = vec![0u8; dict_capacity];
+
+        let params = ZDICT_legacy_params_t {
+            zParams: ZDICT_params_t {
+                compressionLevel: 3,
+                notificationLevel: 0,
+                dictID: 0,
+            },
+            selectivityLevel: 0,
+        };
+
+        let dict_size = ZDICT_trainFromBuffer_legacy(
+            dict_buffer.as_mut_ptr() as *mut c_void,
+            dict_buffer.len(),
+            sample_data.as_ptr() as *const c_void,
+            sample_sizes.as_ptr(),
+            sample_sizes.len() as u32,
+            params,
+        );
+
+        assert_eq!(
+            ZDICT_isError(dict_size),
+            0,
+            "Dict training failed {:?}",
+            CStr::from_ptr(ZDICT_getErrorName(dict_size)).to_str(),
+        );
+
+        dict_buffer.truncate(dict_size);
+
+        println!("Dictionary size: {}", dict_size);
+
+        let cctx = ZSTD_createCCtx();
+
+        assert!(!cctx.is_null());
+
+        let max_compressed_size = ZSTD_compressBound(input_data.len());
+
+        let mut compressed = vec![0u8; max_compressed_size];
+
+        let compressed_size = ZSTD_compress_usingDict(
+            cctx,
+            compressed.as_mut_ptr() as *mut c_void,
+            compressed.len(),
+            input_data.as_bytes().as_ptr() as *const c_void,
+            input_data.len(),
+            dict_buffer.as_ptr() as *const c_void,
+            dict_buffer.len(),
+            3, // compression level
+        );
+
+        assert_eq!(ZSTD_isError(compressed_size), 0, "Compression failed");
+
+        compressed.truncate(compressed_size);
+
+        println!("Compressed size: {}", compressed_size);
+
+        ZSTD_freeCCtx(cctx);
+
+        (compressed, dict_buffer)
+    });
+}
+
+#[test]
+#[cfg(not(target_family = "wasm"))]
 fn test_optimize_train_from_buffer_cover_single_threaded() {
     test_optimize_train_from_buffer_cover_pool(1);
 }
