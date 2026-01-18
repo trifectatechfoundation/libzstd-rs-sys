c8e9eecff2c32c0736e48093104e2937d82da3fc
PeterM <1434309+petesmc@users.noreply.github.com>
2026-01-18 20:13:08 +0800

    Add assertions from huf_compress.c to huf_compress.rs

--- lib/common/huf.rs
+++ lib/common/huf.rs
@@ -16,8 +16,11 @@ pub(crate) const HUF_SYMBOLVALUE_MAX: u32 = 255;
 pub(crate) const HUF_CTABLE_WORKSPACE_SIZE_U32: usize =
     (4 * (HUF_SYMBOLVALUE_MAX as usize + 1)) + 192;
 
+pub(crate) const HUF_CTABLE_WORKSPACE_SIZE: usize =
+    HUF_CTABLE_WORKSPACE_SIZE_U32 * core::mem::size_of::<u32>();
+
 /// Absolute limit of [`HUF_MAX_TABLELOG`]. Beyond that value, code does not work
-const HUF_TABLELOG_ABSOLUTEMAX: usize = 12;
+pub(crate) const HUF_TABLELOG_ABSOLUTEMAX: usize = 12;
 const _: () = assert!(
     HUF_TABLELOG_MAX <= HUF_TABLELOG_ABSOLUTEMAX,
     "HUF_TABLELOG_MAX is too large !"
