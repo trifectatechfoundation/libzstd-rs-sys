5c6a0cd35aef0c18e40dab4994316d1606c18b0c
Michiel <michielp1807@gmail.com>
2026-02-03 11:07:23 +0100

    Use macros to dedup example code

--- test-libzstd-rs-sys/examples/decompress.rs
+++ test-libzstd-rs-sys/examples/decompress.rs
@@ -62,99 +62,30 @@ fn main() {
     }
 }
 
-fn rs(compressed: &[u8]) {
-    use libzstd_rs_sys::lib::{
-        common::zstd_common::{ZSTD_getErrorName, ZSTD_isError},
-        decompress::zstd_decompress::{ZSTD_decompress, ZSTD_getFrameContentSize},
-        zstd::*,
-    };
+macro_rules! decompress {
+    ($compressed:expr) => {
+        let compressed_ptr = $compressed.as_ptr() as *const c_void;
+        let compressed_size = $compressed.len();
 
-    let compressed_ptr = compressed.as_ptr() as *const c_void;
-    let compressed_size = compressed.len();
-
-    // Get decompressed size from frame header
-    let decompressed_size = unsafe { ZSTD_getFrameContentSize(compressed_ptr, compressed_size) };
-    if decompressed_size == ZSTD_CONTENTSIZE_ERROR {
-        panic!("Not a valid zstd compressed frame!");
-    } else if decompressed_size == ZSTD_CONTENTSIZE_UNKNOWN {
-        panic!("Original size unknown — use streaming decompression.");
-    }
-
-    // Allocate buffer for decompressed output
-    let mut decompressed = vec![0u8; decompressed_size as usize];
-    let result = unsafe {
-        ZSTD_decompress(
-            decompressed.as_mut_ptr() as *mut c_void,
-            decompressed_size as usize,
-            compressed_ptr,
-            compressed_size,
-        )
-    };
-
-    // Check for errors
-    if ZSTD_isError(result) != 0 {
-        let err_msg = unsafe {
-            let ptr = ZSTD_getErrorName(result);
-            core::ffi::CStr::from_ptr(ptr)
-                .to_string_lossy()
-                .into_owned()
-        };
-        panic!("Decompression failed: {}", err_msg);
-    }
-}
-
-fn rs_chunked(compressed: &[u8], chunk_size: usize, dict: Option<&[u8]>) {
-    use libzstd_rs_sys::{
-        lib::{
-            common::zstd_common::{ZSTD_getErrorName, ZSTD_isError},
-            decompress::zstd_decompress::ZSTD_getFrameContentSize,
-            zstd::*,
-        },
-        ZSTD_DCtx_refDDict, ZSTD_createDCtx, ZSTD_createDDict, ZSTD_decompressStream,
-    };
-
-    // Allocate and initialize a decompression context
-    let dctx = unsafe { ZSTD_createDCtx() };
-    if dctx.is_null() {
-        panic!("Failed to create DCtx");
-    }
-
-    if let Some(dict) = dict {
-        // Initialize decompression with the dictionary
-        let ddict = unsafe { ZSTD_createDDict(dict.as_ptr() as *const c_void, dict.len()) };
-        if ddict.is_null() {
-            panic!("Failed to create DDict");
+        // Get decompressed size from frame header
+        let decompressed_size =
+            unsafe { ZSTD_getFrameContentSize(compressed_ptr, compressed_size) };
+        if decompressed_size == ZSTD_CONTENTSIZE_ERROR as u64 {
+            panic!("Not a valid zstd compressed frame!");
+        } else if decompressed_size == ZSTD_CONTENTSIZE_UNKNOWN as u64 {
+            panic!("Original size unknown — use streaming decompression.");
         }
 
-        // Reference the dictionary in the decompression context
-        let res = unsafe { ZSTD_DCtx_refDDict(dctx, ddict) };
-        assert_eq!(ZSTD_isError(res), 0, "Failed to reference DDict");
-    }
-
-    // Get decompressed size from frame header
-    let decompressed_size =
-        unsafe { ZSTD_getFrameContentSize(compressed.as_ptr() as *const c_void, chunk_size) };
-    if decompressed_size == ZSTD_CONTENTSIZE_ERROR {
-        panic!("Not a valid zstd compressed frame!");
-    } else if decompressed_size == ZSTD_CONTENTSIZE_UNKNOWN {
-        panic!("Original size unknown — use streaming decompression.");
-    }
-    let mut output_buf = vec![0u8; decompressed_size as usize];
-    let mut output = ZSTD_outBuffer {
-        dst: output_buf.as_mut_ptr() as *mut c_void,
-        size: output_buf.len(),
-        pos: 0,
-    };
-
-    for chunk in compressed.chunks(chunk_size) {
-        let mut input = ZSTD_inBuffer {
-            src: chunk.as_ptr() as *const c_void,
-            size: chunk.len(),
-            pos: 0,
-        };
-
         // Allocate buffer for decompressed output
-        let result = unsafe { ZSTD_decompressStream(dctx, &mut output, &mut input) };
+        let mut decompressed = vec![0u8; decompressed_size as usize];
+        let result = unsafe {
+            ZSTD_decompress(
+                decompressed.as_mut_ptr() as *mut c_void,
+                decompressed_size as usize,
+                compressed_ptr,
+                compressed_size,
+            )
+        };
 
         // Check for errors
         if ZSTD_isError(result) != 0 {
@@ -166,109 +97,100 @@ fn rs_chunked(compressed: &[u8], chunk_size: usize, dict: Option<&[u8]>) {
             };
             panic!("Decompression failed: {}", err_msg);
         }
+    };
+}
 
-        if input.pos < input.size {
-            panic!("Not all input data consumed");
-        }
-    }
+fn rs(compressed: &[u8]) {
+    use libzstd_rs_sys::*;
+
+    decompress!(compressed);
 }
 
 fn c(compressed: &[u8]) {
     use zstd_sys::*;
 
-    let compressed_ptr = compressed.as_ptr() as *const c_void;
-    let compressed_size = compressed.len();
-
-    // Get decompressed size from frame header
-    let decompressed_size = unsafe { ZSTD_getFrameContentSize(compressed_ptr, compressed_size) };
-    if decompressed_size == ZSTD_CONTENTSIZE_ERROR as u64 {
-        panic!("Not a valid zstd compressed frame!");
-    } else if decompressed_size == ZSTD_CONTENTSIZE_UNKNOWN as u64 {
-        panic!("Original size unknown — use streaming decompression.");
+    // `ZSTD_isError` is safe in the Rust version, but unsafe with the C bindings
+    #[allow(unused_unsafe)]
+    unsafe {
+        decompress!(compressed);
     }
+}
 
-    // Allocate buffer for decompressed output
-    let mut decompressed = vec![0u8; decompressed_size as usize];
-    let result = unsafe {
-        ZSTD_decompress(
-            decompressed.as_mut_ptr() as *mut c_void,
-            decompressed_size as usize,
-            compressed_ptr,
-            compressed_size,
-        )
-    };
+macro_rules! decompress_chunks {
+    ($compressed:expr, $chunk_size:expr, $dict:expr) => {
+        // Allocate and initialize a decompression context
+        let dctx = unsafe { ZSTD_createDCtx() };
+        if dctx.is_null() {
+            panic!("Failed to create DCtx");
+        }
 
-    // Check for errors
-    if unsafe { ZSTD_isError(result) } != 0 {
-        let err_msg = unsafe {
-            let ptr = ZSTD_getErrorName(result);
-            core::ffi::CStr::from_ptr(ptr)
-                .to_string_lossy()
-                .into_owned()
+        if let Some(dict) = $dict {
+            // Initialize decompression with the dictionary
+            let ddict = unsafe { ZSTD_createDDict(dict.as_ptr() as *const c_void, dict.len()) };
+            if ddict.is_null() {
+                panic!("Failed to create DDict");
+            }
+
+            // Reference the dictionary in the decompression context
+            let res = unsafe { ZSTD_DCtx_refDDict(dctx, ddict) };
+            assert_eq!(ZSTD_isError(res), 0, "Failed to reference DDict");
+        }
+
+        // Get decompressed size from frame header
+        let decompressed_size =
+            unsafe { ZSTD_getFrameContentSize($compressed.as_ptr() as *const c_void, $chunk_size) };
+        if decompressed_size == ZSTD_CONTENTSIZE_ERROR as u64 {
+            panic!("Not a valid zstd compressed frame!");
+        } else if decompressed_size == ZSTD_CONTENTSIZE_UNKNOWN as u64 {
+            panic!("Original size unknown — use streaming decompression.");
+        }
+        let mut output_buf = vec![0u8; decompressed_size as usize];
+        let mut output = ZSTD_outBuffer {
+            dst: output_buf.as_mut_ptr() as *mut c_void,
+            size: output_buf.len(),
+            pos: 0,
         };
-        panic!("Decompression failed: {}", err_msg);
-    }
+
+        for chunk in $compressed.chunks($chunk_size) {
+            let mut input = ZSTD_inBuffer {
+                src: chunk.as_ptr() as *const c_void,
+                size: chunk.len(),
+                pos: 0,
+            };
+
+            // Allocate buffer for decompressed output
+            let result = unsafe { ZSTD_decompressStream(dctx, &mut output, &mut input) };
+
+            // Check for errors
+            if ZSTD_isError(result) != 0 {
+                let err_msg = unsafe {
+                    let ptr = ZSTD_getErrorName(result);
+                    core::ffi::CStr::from_ptr(ptr)
+                        .to_string_lossy()
+                        .into_owned()
+                };
+                panic!("Decompression failed: {}", err_msg);
+            }
+
+            if input.pos < input.size {
+                panic!("Not all input data consumed");
+            }
+        }
+    };
+}
+
+fn rs_chunked(compressed: &[u8], chunk_size: usize, dict: Option<&[u8]>) {
+    use libzstd_rs_sys::*;
+
+    decompress_chunks!(compressed, chunk_size, dict);
 }
 
 fn c_chunked(compressed: &[u8], chunk_size: usize, dict: Option<&[u8]>) {
     use zstd_sys::*;
 
-    // Allocate and initialize a decompression context
-    let dctx = unsafe { ZSTD_createDCtx() };
-    if dctx.is_null() {
-        panic!("Failed to create DCtx");
-    }
-
-    if let Some(dict) = dict {
-        // Initialize decompression with the dictionary
-        let ddict = unsafe { ZSTD_createDDict(dict.as_ptr() as *const c_void, dict.len()) };
-        if ddict.is_null() {
-            panic!("Failed to create DDict");
-        }
-
-        // Reference the dictionary in the decompression context
-        let res = unsafe { ZSTD_DCtx_refDDict(dctx, ddict) };
-        assert_eq!(unsafe { ZSTD_isError(res) }, 0, "Failed to reference DDict");
-    }
-
-    // Get decompressed size from frame header
-    let decompressed_size =
-        unsafe { ZSTD_getFrameContentSize(compressed.as_ptr() as *const c_void, chunk_size) };
-    if decompressed_size == ZSTD_CONTENTSIZE_ERROR as u64 {
-        panic!("Not a valid zstd compressed frame!");
-    } else if decompressed_size == ZSTD_CONTENTSIZE_UNKNOWN as u64 {
-        panic!("Original size unknown — use streaming decompression.");
-    }
-    let mut output_buf = vec![0u8; decompressed_size as usize];
-    let mut output = ZSTD_outBuffer {
-        dst: output_buf.as_mut_ptr() as *mut c_void,
-        size: output_buf.len(),
-        pos: 0,
-    };
-
-    for chunk in compressed.chunks(chunk_size) {
-        let mut input = ZSTD_inBuffer {
-            src: chunk.as_ptr() as *const c_void,
-            size: chunk.len(),
-            pos: 0,
-        };
-
-        // Allocate buffer for decompressed output
-        let result = unsafe { ZSTD_decompressStream(dctx, &mut output, &mut input) };
-
-        // Check for errors
-        if unsafe { ZSTD_isError(result) } != 0 {
-            let err_msg = unsafe {
-                let ptr = ZSTD_getErrorName(result);
-                core::ffi::CStr::from_ptr(ptr)
-                    .to_string_lossy()
-                    .into_owned()
-            };
-            panic!("Decompression failed: {}", err_msg);
-        }
-
-        if input.pos < input.size {
-            panic!("Not all input data consumed");
-        }
+    // `ZSTD_isError` is safe in the Rust version, but unsafe with the C bindings
+    #[allow(unused_unsafe)]
+    unsafe {
+        decompress_chunks!(compressed, chunk_size, dict);
     }
 }
