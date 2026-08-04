a2543343f8ecd65a92ca5f7a7931eb0b2e130b38
Folkert de Vries <folkert@folkertdev.nl>
2026-08-05 00:57:38 +0200

    `fn ZSTD_getLowestPrefixIndex`: pointer argument to reference

--- lib/compress/zstd_compress_internal.rs
+++ lib/compress/zstd_compress_internal.rs
@@ -350,18 +350,18 @@ pub(crate) unsafe fn ZSTD_getLowestMatchIndex(
 
 #[inline]
 pub(crate) unsafe fn ZSTD_getLowestPrefixIndex(
-    ms: *const ZSTD_MatchState_t,
+    ms: &ZSTD_MatchState_t,
     curr: u32,
     windowLog: core::ffi::c_uint,
 ) -> u32 {
     let maxDistance = 1 << windowLog;
-    let lowestValid = (*ms).window.dictLimit;
+    let lowestValid = ms.window.dictLimit;
     let withinWindow = if curr.wrapping_sub(lowestValid) > maxDistance {
         curr.wrapping_sub(maxDistance)
     } else {
         lowestValid
     };
-    let isDictionary = ((*ms).loadedDictEnd != 0) as core::ffi::c_int as u32;
+    let isDictionary = (ms.loadedDictEnd != 0) as core::ffi::c_int as u32;
 
     if isDictionary != 0 {
         lowestValid
