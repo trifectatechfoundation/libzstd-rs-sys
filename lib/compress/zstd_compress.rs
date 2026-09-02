4afa4d30ed8041485591cf230e40e38f9ad0968f
Folkert de Vries <folkert@folkertdev.nl>
2026-09-02 18:04:53 +0200

    `fn ZSTD_countSeqStoreMatchBytes`: take `seqStore` by reference

--- lib/compress/zstd_compress.rs
+++ lib/compress/zstd_compress.rs
@@ -5881,14 +5881,13 @@ unsafe fn ZSTD_countSeqStoreLiteralsBytes(seqStore: &SeqStore_t) -> size_t {
 }
 
 /// Returns match bytes represented in a seqStore
-unsafe fn ZSTD_countSeqStoreMatchBytes(seqStore: *const SeqStore_t) -> size_t {
+unsafe fn ZSTD_countSeqStoreMatchBytes(seqStore: &SeqStore_t) -> size_t {
     let mut matchBytes = 0usize;
-    let nbSeqs = ((*seqStore).sequences).offset_from((*seqStore).sequencesStart) as size_t;
+    let nbSeqs = (seqStore.sequences).offset_from(seqStore.sequencesStart) as size_t;
     for i in 0..nbSeqs {
-        let seq = *((*seqStore).sequencesStart).add(i);
+        let seq = *(seqStore.sequencesStart).add(i);
         matchBytes = matchBytes.wrapping_add((seq.mlBase as core::ffi::c_int + MINMATCH) as size_t);
-        if i == (*seqStore).longLengthPos as size_t
-            && (*seqStore).longLengthType == LongLengthType::Match
+        if i == seqStore.longLengthPos as size_t && seqStore.longLengthType == LongLengthType::Match
         {
             matchBytes = matchBytes.wrapping_add(0x10000 as core::ffi::c_int as size_t);
         }
