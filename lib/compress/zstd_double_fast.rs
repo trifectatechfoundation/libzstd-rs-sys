95480a905c9baf7d07b4c8c15303e3b30bd5c8e5
Folkert de Vries <folkert@folkertdev.nl>
2026-09-02 18:45:17 +0200

    Dedup `ZSTD_SHORT_CACHE_TAG_MASK` into `zstd_compress_internal`

--- lib/compress/zstd_double_fast.rs
+++ lib/compress/zstd_double_fast.rs
@@ -10,16 +10,13 @@ use crate::lib::compress::zstd_compress::{SeqStore_t, ZSTD_MatchState_t};
 use crate::lib::compress::zstd_compress_internal::{
     DictTableLoadMethod, TableFillPurpose, ZSTD_count, ZSTD_count_2segments,
     ZSTD_getLowestMatchIndex, ZSTD_getLowestPrefixIndex, ZSTD_hashPtr, ZSTD_index_overlap_check,
-    ZSTD_storeSeq, ZSTD_SHORT_CACHE_TAG_BITS,
+    ZSTD_storeSeq, ZSTD_SHORT_CACHE_TAG_BITS, ZSTD_SHORT_CACHE_TAG_MASK,
 };
 use crate::lib::zstd::ZSTD_compressionParameters;
 
 pub const kSearchStrength: core::ffi::c_int = 8;
 pub const HASH_READ_SIZE: core::ffi::c_int = 8;
 pub const REPCODE1_TO_OFFBASE: core::ffi::c_int = 1;
-
-pub const ZSTD_SHORT_CACHE_TAG_MASK: core::ffi::c_uint =
-    ((1 as core::ffi::c_uint) << ZSTD_SHORT_CACHE_TAG_BITS).wrapping_sub(1);
 
 /// Helper function for ZSTD_fillHashTable and ZSTD_fillDoubleHashTable.
 /// Unpacks hashAndTag into (hash, tag), then packs (index, tag) into hashTable[hash].
