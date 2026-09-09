d8a985bfb75dcdfd668576a15d570feef9eb9fc6
Michiel <michielp1807@gmail.com>
2026-09-09 10:29:13 +0200

    Dedup `prime8bytes` into `zstd_compress_internal`

--- lib/compress/zstdmt_compress.rs
+++ lib/compress/zstdmt_compress.rs
@@ -23,8 +23,8 @@ use crate::lib::compress::zstd_compress::{
     ZSTD_writeLastEmptyBlock,
 };
 use crate::lib::compress::zstd_compress_internal::{
-    CParamMode, DictTableLoadMethod, ZSTD_window_clear, ZSTD_window_hasExtDict, ZSTD_window_init,
-    ZSTD_window_update,
+    prime8bytes, CParamMode, DictTableLoadMethod, ZSTD_window_clear, ZSTD_window_hasExtDict,
+    ZSTD_window_init, ZSTD_window_update,
 };
 use crate::lib::compress::zstd_ldm::{
     ldmEntry_t, ldmParams_t, ldmState_t, ZSTD_ldm_adjustParameters, ZSTD_ldm_fillHashTable,
@@ -204,8 +204,6 @@ struct SyncPoint {
 
 const ZSTD_c_forceMaxWindow: ZSTD_cParameter = ZSTD_cParameter::ZSTD_c_experimentalParam3;
 const ZSTD_c_deterministicRefPrefix: ZSTD_cParameter = ZSTD_cParameter::ZSTD_c_experimentalParam15;
-
-static prime8bytes: u64 = 0xcf1bbcdcb7a56463 as core::ffi::c_ulonglong;
 
 /// Return base^exponent
 fn ZSTD_ipow(mut base: u64, mut exponent: u64) -> u64 {
