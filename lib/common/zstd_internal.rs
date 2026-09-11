f4c1bbf518f16154b84d2983ed7f7a83777d3a93
Michiel <michielp1807@gmail.com>
2026-09-11 11:47:58 +0200

    Define and use `ZSTD_MAX_FSE_HEADERS_SIZE`

--- lib/common/zstd_internal.rs
+++ lib/common/zstd_internal.rs
@@ -18,9 +18,6 @@ pub(crate) static repStartValue: RepCodes = [1, 4, 8];
 
 pub(crate) const ZSTD_FRAMEIDSIZE: usize = 4;
 
-/// header + <= 127 byte tree description
-pub(crate) const ZSTD_MAX_HUF_HEADER_SIZE: usize = 128;
-
 pub(crate) const ZSTD_BLOCKHEADERSIZE: usize = 3;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Default)]
@@ -90,6 +87,14 @@ pub(crate) const MaxFSELog: usize = const_max(
 );
 pub(crate) const MaxMLBits: u8 = 16;
 pub(crate) const MaxLLBits: u8 = 16;
+
+/// header + <= 127 byte tree description
+pub(crate) const ZSTD_MAX_HUF_HEADER_SIZE: usize = 128;
+/// Each table cannot take more than #symbols * FSELog bits
+pub(crate) const ZSTD_MAX_FSE_HEADERS_SIZE: usize = ((MaxML as u32 + 1) * MLFSELog
+    + (MaxLL as u32 + 1) * LLFSELog
+    + (MaxOff as u32 + 1) * OffFSELog)
+    .div_ceil(8) as usize;
 
 pub(crate) static LL_bits: [u8; MaxLL as usize + 1] = [
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 3, 3, 4, 6, 7, 8, 9, 10, 11,
